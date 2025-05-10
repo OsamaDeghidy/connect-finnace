afd87ce36960ed0739d21f662d6f90fa44976431
Temporary User <temp@example.com>
2025-05-10 15:21:15 +0300

    3 commit

--- server/finance_system/settings.py
+++ server/finance_system/settings.py
@@ -201,7 +201,7 @@ REST_FRAMEWORK = {
 # JWT Settings
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=900),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
